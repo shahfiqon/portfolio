74fc210b86a5dcb709baf96f72532687b81c6b7e
shahfiqon <jan01789362@gmail.com>
2025-11-04 12:14:21 -0800

    fix: url change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   deploymentBranch: 'gh-pages',
 
   // Set the production url of your site here
-  url: 'https://shahfiqon.github.io',
+  url: 'https://shahfiqon.github.io/portfolio',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
